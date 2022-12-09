35b51be24cb0126693dd26e2fd27fa25ebc5fd98
Paulo Roberto <56531195+PauloRoberto2@users.noreply.github.com>
2022-12-09 15:49:48 -0300

    Add files via upload

---
+++ ExtremamenteBasico_1001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double A;
+        double B;
+        double X;
+        A = double.Parse(Console.ReadLine());
+        B = double.Parse(Console.ReadLine());
+        X = A + B;
+        Console.WriteLine($"X = {X}");
+    }
+
+}
