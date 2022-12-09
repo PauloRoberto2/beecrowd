ef9c3870028c980d013f317a5c63f2e6e4b8c1f0
Paulo Roberto <paulo22game22@gmail.com>
2022-12-09 20:09:22 -0300

    pra cima deles gogo

--- DistanciaEntreDoisPontos_1015/Program.cs
+++ DistanciaEntreDoisPontos_1015/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string[] line1 = Console.ReadLine().Split(' ');
+        string[] line2 = Console.ReadLine().Split(' ');
+        double x1 = double.Parse(line1[0]);
+        double y1 = double.Parse(line1[1]);
+        double x2 = double.Parse(line2[0]);
+        double y2 = double.Parse(line2[1]);
+
+        double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+        Console.WriteLine($"{distance:f4}");
+
+    }
+
+}
