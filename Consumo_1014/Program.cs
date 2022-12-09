a7722fb536ba7a55b77716b35869a2ca60ee1d67
Paulo Roberto <paulo22game22@gmail.com>
2022-12-09 19:51:04 -0300

    let's go

--- Consumo_1014/Program.cs
+++ Consumo_1014/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X = int.Parse(Console.ReadLine());
+        double Y = double.Parse(Console.ReadLine());
+
+        double consumption = X / Y;
+
+        Console.WriteLine($"{consumption:f3} km/l");
+    }
+
+}
