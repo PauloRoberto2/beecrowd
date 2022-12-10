cbcc6d09d20f2c835ebdc1ed1630dc5a661a6079
Paulo Roberto <paulo22game22@gmail.com>
2022-12-09 21:04:53 -0300

    ae

--- GastodeCombustivel_1017/Program.cs
+++ GastodeCombustivel_1017/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        
+        int time = int.Parse(Console.ReadLine());
+        int average = int.Parse(Console.ReadLine());
+
+        double litro = (time * average) / 12.00;
+
+        Console.WriteLine($"{litro:f3}");
+    }
+}
