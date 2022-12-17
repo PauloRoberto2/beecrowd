5af4a357a30e679f51c370f65f965abbf1b0f104
Paulo Roberto <paulo22game22@gmail.com>
2022-12-17 20:28:27 -0300

    work

--- ConversaodeTempo_1019/Program.cs
+++ ConversaodeTempo_1019/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+class Program {
+
+    static void Main(string[] args) { 
+
+        int H_S = 3600;
+
+        int time = int.Parse(Console.ReadLine());
+        
+        int hour = time / H_S;
+        int min = (time - (H_S * hour)) / 60;
+        int sec = time - (H_S * hour) - (min * 60);
+
+        Console.WriteLine($"{hour}:{min}:{sec}");
+
+    }
+
+}   
