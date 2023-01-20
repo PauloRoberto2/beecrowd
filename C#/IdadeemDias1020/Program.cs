11f627e91d2a475c022e2db8a33a1cbca575361d
Paulo Roberto <paulo22game22@gmail.com>
2023-01-20 18:34:17 -0300

    build

--- C#/IdadeemDias1020/Program.cs
+++ C#/IdadeemDias1020/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int idadeDias, anos, meses, dias;
+
+    //Lendo a idade em dias
+    idadeDias = int.Parse(Console.ReadLine());
+
+    //Calculando a idade em anos, meses e dias
+    anos = idadeDias / 365;
+    meses = (idadeDias % 365) / 30;
+    dias = (idadeDias % 365) % 30;
+
+    //Imprimindo a idade em anos, meses e dias
+    Console.WriteLine(anos + " ano(s)");
+    Console.WriteLine(meses + " mes(es)");
+    Console.WriteLine(dias + " dia(s)");
+  }
+}
